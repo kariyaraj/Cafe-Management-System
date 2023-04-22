3b6cba1f47ff5f2e9b34b15290d7a6ed5b922b07
kariyaraj <101118212+kariyaraj@users.noreply.github.com>
2023-04-22 09:52:41 +0530

    Add files via upload

---
+++ dashboard/dashboard.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard.component';
+import { DashboardRoutes } from './dashboard.routing';
+import { MaterialModule } from '../shared/material-module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule,
+    RouterModule.forChild(DashboardRoutes)
+  ],
+  declarations: [DashboardComponent]
+})
+export class DashboardModule { }
